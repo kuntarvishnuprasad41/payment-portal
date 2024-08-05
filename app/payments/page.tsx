354a98c888ce8c01b9217bb5772e9b5b7095f688
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-08-05 21:41:29 +0530

    init

---
+++ app/payments/page.tsx
@@ -0,0 +1,12 @@
+import { Payments } from "@/components/payments";
+import React from "react";
+
+function page() {
+  return (
+    <div className="bg-slate-700">
+      <Payments />
+    </div>
+  );
+}
+
+export default page;
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['api.vishnuprasadkuntar.me'],
+    },
+};
+
+export default nextConfig;
